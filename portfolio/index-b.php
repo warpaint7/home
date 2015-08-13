d2a8008c928f53568ba4f3ada7183492515162cd
Warren Curtis <warren.curtis@iamplus.com>
2015-08-13 14:18:37 -0700

    first commit

---
+++ portfolio/index-b.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Warren Curtis | Portfolio</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    
+     <div class="fixed contain-to-grid">
+    <nav class="top-bar" data-topbar role="navigation"> <ul class="title-area"> <li class="name"> <img class="logo" src="img/warren-curtis-logo.png" alt="warren logo"></li> <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->  <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li> </ul> <section class="top-bar-section">  <!-- right-col Nav Section --> <ul class="right"> <li class="active"><a href="img/WarrenCurtis_resume.pdf">Download Resume</a></li></section> </nav>
+
+</div> <!-- nav -->
+    
+    <div class="row">
+      <div class="large-12 columns">
+      
+	        <div class="row">
+	        	<div class="large-3 medium-3 columns text-center">
+	    		<a href="#" data-reveal-id="ModalOne"><img src="img/rm-1_t.jpg" alt="design"></a>
+	    		<a class="sub-link" href="#" data-reveal-id="ModalOne">Realty Mogul Investment Details</a>
+	    	</div>
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalTwo"><img src="img/rm-2_t.jpg" alt="design"></a>	
+	        		<a class="sub-link" href="#" data-reveal-id="ModalTwo">Realty Mogul Get a Loan Form</a>
+	        		</div>
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalFour"><img src="img/rm-menu.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalFour">Realty Mogul Navigation</a>
+	        	</div>    
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalThree"><img src="img/rm-hardrock_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="Modalthree">Realty Mogul Hard Rock Hotel</a>
+	        	</div>     
+					</div>
+      	
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="large-12 columns">
+      
+	        <div class="row">
+	        
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalFive"><img src="img/aud_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalFive">Audyssey Product Page</a>
+	        	</div>     
+	        		        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalSix"><img src="img/ride-mobile_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSix">Ride to Conquer Cancer Mobile</a>
+
+	        	</div> 
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalSeven"><img src="img/beacon_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSeven">Beacon iphone App Concept</a>
+
+	        	</div>  
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalEight"><img src="img/chrisgore_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalEight">Chris Gore Website</a>
+
+	        	</div>            
+					</div>
+      	
+      </div>
+    </div>
+
+
+     <div class="row">
+      <div class="large-12 columns" style="display: none;">
+      
+	        <div class="row">
+	        
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalNine"><img src="img/ride-routes_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalFive">The Ride: Route Options</a>
+	        	</div>     
+	        
+				<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalSix"><img src="img/ride-mobile_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSix">Ride to Conquer Cancer Mobile</a>
+
+	        	</div> 
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalSeven"><img src="img/beacon_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSeven">Beacon iphone App Concept</a>
+
+	        	</div>  
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalEight"><img src="img/chrisgore_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalEight">Chris Gore Website</a>
+
+	        	</div>            
+					</div>
+      	
+      </div>
+    </div>
+
+
+
+
+
+   
+    
+    
+    <!-- modal 1  -->
+    <div id="ModalOne" class="reveal-modal" data-reveal>
+  <div class="left-col"><img width="600px" src="img/debt-FINAL.jpg"></div>
+  <div class="right-col"><h4>Realty Mogul: Investment Opportunity Debt Details</h4>
+  <p class="margin-top">Part of the Realty Mogul application rebranding project.</p><p><strong>Objective: </strong>improve UI so investors can easily find the details about properties that they would like to invest in.</p> <p>Collaborated with Stakeholders and developers to translate wireframe to finished design. Based on 960 grid for layout. Includes rebranded logo and menu in the header.</p>
+  </div>
+  
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 1  -->
+    
+     <!-- modal 2  -->
+    <div id="ModalTwo" class="reveal-modal" data-reveal>
+	  <div class="left-col"> <img src="img/rehab-loan-app.png"></div>
+  <div class="right-col"><h4>Realty Mogul: Residential Pre qualification form</h4>
+ <p>Part of the Realty Mogul application rebranding project.</p><p><strong>Objective: </strong>Design a one page form for individuals who would like to get a loan for financing a commercial real estate project.</p> <p>Collaborated with Stakeholders and developers to redesign the previous application which was not converting users.</p>
+  </div>
+ 
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 2  -->
+    
+      <!-- modal 3  -->
+    <div id="ModalThree" class="reveal-modal" data-reveal>
+	    <div class="left-col"> <img src="img/RM-Hardrock.jpg"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul: Hard Rock Hotel Palm Springs</h4>
+  <p>Design a details page to promote the opportunity to crowd-fund the Hard Rock Hotel Palm Springs. </p><p><strong>Objective: </strong>Project included the design of ad banners, homepage banners and other marketing materials.</p> <p>Collaborated Hard Rock Corporate and franchise stakeholders to design all assets for web and print.</p>
+	    </div>
+  
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 3  -->
+    
+    <!-- modal 4  -->
+    <div id="ModalFour" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/realty-mogul-nav.png"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul: Rebrand, updated menu concept</h4>
+  <p>Redesign top navigation to improve user flow. </p><p><strong>Objective: </strong>Improve the websites top navigation to allow users to access everything in the site flow from any page. </p> <p>Create site maps, determine user actions based on Google Analytics research. </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 4  -->
+    
+    <!-- modal 5  -->
+    <div id="ModalFive" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/audyssey_web_v1.png"></div>
+	    <div class="right-col">
+  <h4>Audyssey: Lower East Side Speaker Dock Web Design</h4>
+  <p><strong>Objective: </strong>Design page to promote speaker dock</p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 5  -->
+
+      <!-- modal 6  -->
+    <div id="ModalSix" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/ride-mobile.jpg"></div>
+	    <div class="right-col">
+  <h4>Ride to Conquer Cancer: Mobile Splash</h4>
+  <p><strong>Objective: </strong>Create a simple mobile splash page for the Ride to Conquer Cancer allowing for users to select different cities and request more information. </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 6  -->
+    
+      <!-- modal 7  -->
+    <div id="ModalSeven" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/beacon-full.png"></div>
+	    <div class="right-col">
+  <h4>Beacon: iphone App Concept</h4>
+  <p><strong>Idea: </strong>Design a mobile app that allows your phone to act as a beacon to alert friends and family that the user is at a location near them so everyone can connect.  </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 7  -->
+    
+    <!-- modal 8  -->
+    <div id="ModalEight" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/That-Chris-Gore.jpg"></div>
+	    <div class="right-col">
+  <h4>Chris Gore Website design</h4>
+  <p><strong>Objective: </strong>Design a simple website for Chris to use a Tumblr theme.  </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 8  -->
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
---
+++ portfolio/index.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Warren Curtis | Portfolio</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    
+     <div class="fixed contain-to-grid">
+    <nav class="top-bar" data-topbar role="navigation"> <ul class="title-area"> <li class="name"> <img class="logo" src="img/warren-curtis-logo.png" alt="warren logo"></li> <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->  <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li> </ul> <section class="top-bar-section">  <!-- right-col Nav Section --> <ul class="right"> <li class="active"><a href="img/WarrenCurtis_resume.pdf">Download Resume</a></li></section> </nav>
+
+</div> <!-- nav -->
+    
+    <div class="row">
+      <div class="large-12 columns">
+      
+	        <div class="row">
+	        	<div class="large-3 medium-3 columns text-center">
+	    		<a href="#" data-reveal-id="ModalTen"><img src="img/brand.png" alt="design"></a>
+	    		<a class="sub-link" href="#" data-reveal-id="ModalOne">Realty Mogul Logo and Branding</a>
+	    	</div>
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalNine"><img src="img/browse-page2_t.jpg" alt="design"></a>	
+	        		<a class="sub-link" href="#" data-reveal-id="ModalTwo">Realty Mogul Investment Browse</a>
+	        		</div>
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalEleven"><img src="img/rm-mobile_t.png" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalFour">Realty Mogul Responsive</a>
+	        	</div>    
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalOne"><img src="img/rm-1_t.jpg" alt="design"></a>
+	    		<a class="sub-link" href="#" data-reveal-id="ModalOne">Realty Mogul Investment Details</a>
+	        	</div>     
+					</div>
+      	
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="large-12 columns">
+      
+	        <div class="row">
+	        
+	        
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalTwo"><img src="img/rm-2_t.jpg" alt="design"></a>
+	    		<a class="sub-link" href="#" data-reveal-id="ModalTwo">Realty Mogul Residential Pre-qualification form</a>
+
+	        	</div> 
+	        	
+	        	
+	        	   
+	        		        
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalTwelve"><img src="img/fullapp_t.png" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSeven">Realty Mogul Residential Full Application form</a>
+
+	        	</div>  
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+		        	<a href="#" data-reveal-id="ModalThree"><img src="img/rm-hardrock_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="Modalthree">Realty Mogul Hard Rock Hotel</a>
+	        		
+	        		
+	        	</div>  
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalEight"><img src="img/chrisgore_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalEight">Chris Gore Website</a>
+
+	        	</div>            
+					</div>
+			
+      	
+      </div>
+    </div>
+
+
+     <div class="row">
+      <div class="large-12 columns lastrow">
+      
+	        <div class="row">
+	        
+	        
+	        	  
+	        		        
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalSeven"><img src="img/wireframes_t.png" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSeven">Wireframes</a>
+
+	        	</div>  
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalFive"><img src="img/aud_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalFive">Audyssey Product Page</a>
+	        	</div>   
+	        
+				<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalSix"><img src="img/ride-mobile_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSix">Ride to Conquer Cancer Mobile</a>
+
+	        	</div> 
+	        	
+	        	<div class="large-3 medium-3 columns text-center">
+	        		<a href="#" data-reveal-id="ModalThirteen"><img src="img/beacon_t.jpg" alt="design"></a>
+	        		<a class="sub-link" href="#" data-reveal-id="ModalSeven">Beacon iphone App Concept</a>
+
+	        	</div>            
+					</div>
+			
+      	
+      </div>
+    </div>
+
+
+
+
+
+   
+    
+    
+    <!-- modal 1  -->
+    <div id="ModalOne" class="reveal-modal" data-reveal>
+  <div class="left-col"><img width="600px" src="img/debt-FINAL.jpg"></div>
+  <div class="right-col"><h4>Realty Mogul: Investment Opportunity Debt Details</h4>
+  <p class="margin-top">Part of the Realty Mogul application rebranding project.</p><p><strong>Objective: </strong>improve UI so investors can easily find the details about properties that they would like to invest in.</p> <p>Collaborated with Stakeholders and developers to translate wireframe to finished design. Based on 960 grid for layout. Includes rebranded logo and menu in the header.</p>
+  </div>
+  
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 1  -->
+    
+     <!-- modal 2  -->
+    <div id="ModalTwo" class="reveal-modal" data-reveal>
+	  <div class="left-col"> <img src="img/rehab-loan-app.png"><br /><br /><img src="img/thankyou-loan.png"></div>
+  <div class="right-col"><h4>Realty Mogul: Residential Pre qualification form</h4>
+ <p>Part of the Realty Mogul application rebranding project.</p><p><strong>Objective: </strong>Design a one page form for individuals who would like to get a loan for financing a commercial real estate project.</p> <p>Collaborated with Stakeholders and developers to redesign the previous application which was not converting users.</p>
+  </div>
+ 
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 2  -->
+    
+      <!-- modal 3  -->
+    <div id="ModalThree" class="reveal-modal" data-reveal>
+	    <div class="left-col"> <img src="img/RM-Hardrock.jpg"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul: Hard Rock Hotel Palm Springs</h4>
+  <p>Design a details page to promote the opportunity to crowd-fund the Hard Rock Hotel Palm Springs. </p><p><strong>Objective: </strong>Project included the design of ad banners, homepage banners and other marketing materials.</p> <p>Collaborated Hard Rock Corporate and franchise stakeholders to design all assets for web and print.</p>
+	    </div>
+  
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 3  -->
+    
+    <!-- modal 4  -->
+    <div id="ModalFour" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/realty-mogul-nav.png"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul: Rebrand, updated menu concept</h4>
+  <p>Redesign top navigation to improve user flow. </p><p><strong>Objective: </strong>Improve the websites top navigation to allow users to access everything in the site flow from any page. </p> <p>Create site maps, determine user actions based on Google Analytics research. </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 4  -->
+    
+    <!-- modal 5  -->
+    <div id="ModalFive" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/audyssey_web_v1.png"></div>
+	    <div class="right-col">
+  <h4>Audyssey: Lower East Side Speaker Dock Web Design</h4>
+  <p><strong>Objective: </strong>Design page to promote speaker dock</p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 5  -->
+
+      <!-- modal 6  -->
+    <div id="ModalSix" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/ride-mobile.jpg"></div>
+	    <div class="right-col">
+  <h4>Ride to Conquer Cancer: Mobile Splash</h4>
+  <p><strong>Objective: </strong>Create a simple mobile splash page for the Ride to Conquer Cancer allowing for users to select different cities and request more information. </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 6  -->
+    
+      <!-- modal 7  -->
+    <div id="ModalSeven" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/aboutus.png"><br /><br /><img src="img/appv1.png"><br /><br /><img src="img/browsepagev1.png"><br /><br /><img src="img/cooloffv11.png"><br /><br /><img src="img/sponsorshowcasev1.png"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul: Wireframes</h4>
+  <p><strong>Various Projects</strong>  </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 7  -->
+    
+    <!-- modal 8  -->
+    <div id="ModalEight" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/That-Chris-Gore.jpg"></div>
+	    <div class="right-col">
+  <h4>Chris Gore Website design</h4>
+  <p><strong>Objective: </strong>Design a simple website for Chris to use a Tumblr theme.  </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 8  -->
+    
+    
+    <!-- modal 9  -->
+    <div id="ModalNine" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/browse-page2.jpg"><br /><br /><img src="img/browse-page2-filters.jpg"><br /><br /><img src="img/browse-page2-overlay.jpg"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul Investment Browse Page</h4>
+  <p><strong>Objective: </strong>Simplify the Browse page UI and UX to help users can easily understand the investment they are making. Update page styles to match brand refresh. </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 9  -->
+    
+    
+    
+    
+      <!-- modal 10  -->
+    <div id="ModalTen" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img style="border-style: solid; border-width: 1px; border-color: #efefef;" src="img/branding.jpg"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul Logo refresh.</h4>
+  <p><strong>Objective: </strong>Redesign Realty Mogul's Logo</p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 10  -->
+    
+     <!-- modal 11  -->
+    <div id="ModalEleven" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/rm-mobile-tablet.jpg"><br /><br /><img src="img/rm-mobile-nomenu.jpg"><br /><br /><img src="img/rm-mobile-menu.jpg"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul Responsive Design</h4>
+  <p><strong>Objective: </strong>Redesign Realty Mogul's Homepage so it can be viewed on multiple mobile and tablet devices.</p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 11  -->
+    
+    
+     <!-- modal 12  -->
+    <div id="ModalTwelve" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/fullapp1.png"><br /><br /><img src="img/fullapp2.png"><br /><br /><img src="img/fullapp3.png"></div>
+	    <div class="right-col">
+  <h4>Realty Mogul Full Residential Application</h4>
+  <p><strong>Objective: </strong>Design Full Borrower Experience for residential debt.</p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 12  -->
+    
+     <!-- modal 13  -->
+    <div id="ModalThirteen" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/beacon-full.png"></div>
+	    <div class="right-col">
+  <h4>Beacon: iphone App Concept</h4>
+  <p><strong>Idea: </strong>Design a mobile app that allows your phone to act as a beacon to alert friends and family that the user is at a location near them so everyone can connect.  </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 13  -->
+    
+    
+     <!-- modal 14  -->
+    <div id="ModalFourteen" class="reveal-modal" data-reveal>
+	     <div class="left-col"> <img src="img/ride-mobile.jpg"></div>
+	    <div class="right-col">
+  <h4>Ride to Conquer Cancer: Mobile Splash</h4>
+  <p><strong>Objective: </strong>Create a simple mobile splash page for the Ride to Conquer Cancer allowing for users to select different cities and request more information. </p>
+	    </div>
+
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+    <!-- modal 14  -->
+    
+    
+    
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
